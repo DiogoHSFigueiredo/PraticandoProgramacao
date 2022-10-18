76bf2b360299f81f50c99a2da9598937536680f7
Diogo Figueiredo <diogohsfigueiredo@gmail.com>
2022-10-18 20:42:39 -0300

    Primeiro Commit a partir do NetBeans com novas classes Enum e EnumTest

---
+++ src/exelivrodeitelcap8/EnumTest.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exelivrodeitelcap8;
+
+import java.util.EnumSet;
+
+/**
+ *
+ * @author Diogo
+ */
+//Testando o tipo enum Book.
+public class EnumTest {
+
+    public static void main(String[] args) {
+        System.out.println("All books:");
+
+        //imprime todos os livros em enum Book
+        for (Book book : Book.values()) {
+            System.out.printf("%-10s%-45s%s%n", book, book.getTitle(), book.getCopyrightYear());
+        }
+
+        System.out.printf("%nDisplay a range of enum constants:%n");
+
+        // imprime os primeiros quatro livros
+        for (Book book : EnumSet.range(Book.JHTP, Book.CPPHTP)) 
+            System.out.printf("%-10s%-45s%s%n", book, book.getTitle(), book.getCopyrightYear());
+        
+
+    }
+
+} // fim da classe 
