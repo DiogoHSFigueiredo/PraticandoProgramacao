1d724f3c24a97a3e8d26347bddda8b7ff5379cfa
DiogoHSFigueiredo <diogohsfigueiredo@gmail.com>
2024-05-27 18:30:18 -0300

    Exercicios simplesmente

---
+++ src/exelivrodeitelcap22/GridBagDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exelivrodeitelcap22;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Diogo
+ */
+public class GridBagDemo {
+
+    public static void main(String[] args) {
+
+        GridBagFrame gridBagFrame = new GridBagFrame();
+        gridBagFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gridBagFrame.setSize(300, 150);
+        gridBagFrame.setVisible(true);
+    }
+}
